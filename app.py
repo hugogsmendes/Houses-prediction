a872875009a71e157b81121001e649cb67e8215e
Hugo Mendes <hugogs.mendes@gmail.com>
2026-02-08 11:55:14 -0300

    Implementei lógicas de sessão do usuário com ferramentes do Stremlit

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def login (username: str, password: str):
     try:
         response = requests.post(url, 
                                 data = {"username": username, "password": password},
-                                headers={"Content-Type": "application/x-www-form-urlencoded"},
+                                headers = {"Content-Type": "application/x-www-form-urlencoded"},
                                 timeout = 10)
         
         response.raise_for_status()
@@ -17,28 +17,80 @@ def login (username: str, password: str):
     except Exception as err:
         return {"error": str(err)}
     
+def list_users (tokens: dict):
+    url = "http://127.0.0.1:8000/api/v1/admin/list"
+
+    try:
+
+        response = requests.get(url,
+                                headers = {"Authorization": f"Bearer {tokens["access_token"]}"},
+                                timeout = 10)
+        
+        response.raise_for_status()
+        return response.json()
+    
+    except Exception as err:
+        return {"error": str(err)}
+    
+if "tokens" not in st.session_state:
+    st.session_state.tokens = None
+if "logged_in" not in st.session_state:
+    st.session_state.logged_in = False
+if "current_user" not in st.session_state:
+    st.session_state.current_user = None
+if "users" not in st.session_state:
+    st.session_state.users =  None
+
+
 with st.sidebar:
 
-    st.title("🔒 Login")
+    show_user = st.session_state.current_user or "Faça login"
+    st.markdown(f"**User:** {show_user}")
 
-    st.markdown("---")
+    if st.session_state.tokens and st.session_state.logged_in:
+        st.success(f"Você já está logado")
+        st.caption("Tokens guardados na sessão.")
 
-    username = st.text_input("Username", type = "default")
-    password = st.text_input("Senha", type = "password")
+        if st.button("Listar usuários"):
+            result = list_users(st.session_state.tokens)
+            if "error" in result:
+                st.session_state.users = None
+            else:
+                st.session_state.users = result
 
-    button = st.button("Login")
 
-    if button and (username and password):
-        result = login(username, password)
-        if "error" in result:
-            st.sidebar.error("Credencias inválidas")
-        else:
-            st.sidebar.success("Login OK")
+        if st.button("Logout"):
+            st.session_state.tokens = None
+            st.session_state.logged_in = False
+            st.session_state.current_user = None
+            st.session_state.users = None
+            st.rerun()
+    else:
+        st.title("🔒 Login")
+        st.markdown("---")
+
+        username = st.text_input("Username", type = "default", key = "username")
+        password = st.text_input("Senha", type = "password")
+        button = st.button("Login")
         
+        if button and not(username and password):
+            st.warning("Preencha o username e a senha")
 
+        if button and (username and password):
+            result = login(username, password)
 
-    if button and not(username and password):
-        st.warning("Preencha o username e a senha")
+            if "error" in result:
+                st.sidebar.error("Credencias inválidas")
+            else:
+                st.sidebar.success("Login OK")
+                st.session_state.tokens = {
+                    "access_token": result["access_token"],
+                    "refresh_token": result["refresh_token"]
+                }
+                st.session_state.logged_in = True
+                st.session_state.current_user = username
+                st.rerun()
+        
 
     st.markdown("---")
 
@@ -46,3 +98,15 @@ with st.sidebar:
     with st.sidebar.expander("🔗 Links de contato", expanded = False):
         st.link_button("GitHub", "https://github.com/hugogsmendes")
         st.link_button("Linkedin", "https://www.linkedin.com/in/hugogsmendes/")
+
+if st.session_state.logged_in:
+
+    st.title("🏠 Project Houses Predicition")
+    st.subheader("🖩 Inferência do preço de uma casa através de uma API FastAPI")
+
+    if st.session_state.users is not None:
+        st.subheader("👥 Usuários")
+        if isinstance(st.session_state.users, list):
+            st.dataframe(st.session_state.users, width = "stretch")
+        else:
+            st.json(st.session_state.users)
