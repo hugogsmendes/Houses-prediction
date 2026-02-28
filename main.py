2de33f4d1058a97d557e69315f0c32b509ee5f92
Hugo Mendes <hugogs.mendes@gmail.com>
2026-02-28 17:14:12 -0300

    Implementando CORS no projeto

--- main.py
+++ main.py
@@ -2,12 +2,28 @@ from fastapi import FastAPI
 from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
 from slowapi.errors import RateLimitExceeded
+from fastapi.middleware.cors import CORSMiddleware
 
-limiter = Limiter(key_func=get_remote_address)
-app = FastAPI()
+limiter = Limiter(key_func = get_remote_address)
+app = FastAPI(
+    title = "Houses Predicition",
+    version = "1.0.0"
+)
 app.state.limiter = limiter
 app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
+origins = [
+    "https://houses-prediction.streamlit.app"
+    "http://127.0.0.1:8000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 from api.routes.user_routes import user_router
 from api.routes.adm_routes import adm_router
